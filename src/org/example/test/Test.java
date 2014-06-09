a05b350047b839b75f1fcb757dff0dad25afa39c
torjohn <tor.johnson@cert.se>
2014-06-09 21:38:16 +0200

    v1.0.1 test

--- src/org/example/test/Test.java
+++ src/org/example/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.print("Hello World! (in Test)");
         System.out.print("v1.0.0");
+        System.out.print("v1.0.1");
     }
 
 }
