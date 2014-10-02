8c71aad90462ab0b5bc106bef75fa658c7abf8f0
Tor Johnson <tor.johnson@cert.se>
2014-10-02 13:02:00 +0200

    Update Test2.java
    Line added.

--- src/org/example/test/Test2.java
+++ src/org/example/test/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
         System.out.print("Hello World! (in Test2)");
         System.out.print("v1.0.0");
         System.out.print("Test");
+        System.out.print("Test2");
     }
 
 }
