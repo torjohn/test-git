16997edd650edb8e9dabbb7f666633b8ca66bdc7
torjohn <tor.johnson@cert.se>
2014-10-02 09:40:33 +0200

    Line added.

--- src/org/example/test/Test2.java
+++ src/org/example/test/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.print("Hello World! (in Test2)");
         System.out.print("v1.0.0");
+        System.out.print("Test");
     }
 
 }
